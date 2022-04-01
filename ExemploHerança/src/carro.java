70245429cb0bbfd5a572ff466e7782de22c75d75
aluno <aluno@DSN1251230709>
2022-04-01 14:32:08 -0300

    3° commit

---
+++ ExemploHerança/src/carro.java
@@ -0,0 +1,28 @@
+
+public class carro  extends veiculo{
+
+
+	private boolean quatroPortas;
+	
+	public boolean isQuatroPortas() {
+		
+		return quatroPortas;
+		
+	}
+	
+	public void setQuatroPortas(boolean quatroPortas) {
+		
+		this.quatroPortas = quatroPortas;
+		
+	}
+	
+	@Override
+	public void imprimirDados() {
+		// TODO Auto-generated method stub
+		super.imprimirDados();
+		
+		System.out.println("Quatro Portas: " + quatroPortas);
+		
+	}
+	
+}
---
+++ ExemploHerança/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		carro carro1 = new carro();
+		
+		carro1.setAno(2003);
+		carro1.setMarca("Chevrolet");
+		carro1.setModelo("Astra");
+		carro1.setQuatroPortas(true);
+		
+	}
+	
+}
